d0e86c1403b185ebce98c514ab4a55e7af0dbf92
edwin.janssen <edwin.janssen@roland.com>
2016-02-25 21:28:09 +0100

    VolumeCalculator

---
+++ src/Shapes/Services/VolumeCalculator.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Shapes\Services;
+
+/**
+ * Class VolumeCalculator
+ *
+ * @author  Edwin Janssen
+ *
+ * @package Shapes\Services
+ */
+class VolumeCalculator extends AreaCalculator
+{
+    /**
+     * Sum shape volumes
+     *
+     * @author Edwin Janssen
+     *
+     * @return number
+     */
+    public function sum()
+    {
+        //Todo: implement logic to calculate the volumes and then return value
+        return 0.00;
+    }
+}
